aae590a02288dd2030711e1b110157a547bf2849
Amrit Clair <amrit_clair@hotmail.com>
2023-01-14 15:27:14 -0500

    myFlaskApp (Video 1 of 5)

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id': 1, 
+            'title': 'Article One',
+            'body': 'lorem asfafg fasDFASDGF ADGFSADGASD GSADGFADGEAD DSAAGDAGFASFAS DFAF DASF AFDSFD AFA',
+            'author': 'Amrit Clair', 
+            'create_date': '04-25-2017'
+        
+        }, 
+        {
+            'id': 2, 
+            'title': 'Article Two',
+            'body': 'lorem asfafg fasDFASDGF ADGFSADGASD GSADGFADGEAD DSAAGDAGFASFAS DFAF DASF AFDSFD AFA',
+            'author': 'John Doe', 
+            'create_date': '04-25-2017'
+        
+        }, 
+        {
+            'id': 3, 
+            'title': 'Article Three',
+            'body': 'lorem asfafg fasDFASDGF ADGFSADGASD GSADGFADGEAD DSAAGDAGFASFAS DFAF DASF AFDSFD AFA',
+            'author': 'Michael Smith', 
+            'create_date': '04-25-2017'
+        
+        }
+    ]
+
+    return articles
